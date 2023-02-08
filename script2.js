e263ac0dad420e5b5fe116174d8b5002a035cbda
lapablo <116307378+lapablo@users.noreply.github.com>
2023-02-08 12:51:55 -0300

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,50 @@
+const url_image = 'https://cdn.appanimeplus.tk/img/'
+
+const urlParams = new URLSearchParams(window.location.search)
+
+const name_anime = urlParams.get('a')
+
+async function anime() {
+    const url = `https://appanimeplus.tk/play-api.php?search=${name_anime}`
+
+    const resp = await fetch(url)
+
+    const data = await resp.json()
+
+    const body = document.getElementById('content-result')
+
+    console.log(data)
+
+    if (data == null) {
+        body.innerHTML = '<div id="spam">ops nao encotamos nada <br> <a href="index.html">voltar para home</a> </div>'
+    } else {
+
+        data.map((anime) => {
+            const div = document.createElement('div')
+
+            const img = document.createElement('img')
+
+            img.src = url_image + anime.category_image
+
+            div.append(img)
+
+            const name = document.createElement('h1')
+
+            name.innerText = anime.category_name
+
+            div.append(name)
+
+            function clicou() {
+                window.location.href = `anime.html?a=${anime.id}`
+            }
+
+            div.addEventListener('click', clicou)
+
+            div.classList.add('anime')
+
+            body.append(div)
+        })
+    }
+}
+
+anime()
